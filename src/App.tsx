70784e0ef3549c56d004479ca87a4a423ddc80bb
Arnab Kumar Datta <arnabkum@gmail.com>
2022-08-04 11:39:59 +0200

    initial prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Box,
+  Grid,
+  IconButton,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import QRCode from "react-qr-code";
+import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState<string | null>();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+      >
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Convert your url to a QR code
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-basic"
+            label="Your url"
+            variant="outlined"
+            multiline
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          {url && QrContainer(url)}
+        </Grid>
+      </Grid>
+    </>
   );
+}
+
+function QrContainer(url: string) {
+  return <QRCode value={url}></QRCode>;
 }
 
 export default App;
